e98980d614ab77128404e7e23285d4e077fe157c
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-27 14:16:02 +0600

    feat: added redux middelware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
