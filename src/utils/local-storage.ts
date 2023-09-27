5642136700639cdb2ae662cfc3957e2db58d73de
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-27 15:47:40 +0600

    get token in local storage

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -4,3 +4,9 @@ export const setToLocalStorage = (key: string, value: string) => {
   }
   localStorage.setItem(key, value);
 };
+export const getFromLocalStorage = (key: string) => {
+  if (!key || typeof window === undefined) {
+    return "";
+  }
+  localStorage.getItem(key);
+};
